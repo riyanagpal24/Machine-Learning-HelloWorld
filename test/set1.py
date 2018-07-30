298e6f778a9e85854bad982635f0fb62aeb89626
riyanagpal24 <riyanagpal1009@gmail.com>
2018-07-30 16:24:26 +0530

    review analyses initial commit

---
+++ test/set1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 30 15:57:49 2018
+
+@author: riya.nagpal
+"""
+'''
+     to analyse the reviews and predict whether it is recommmended
+     by the consumer or not.
+ 
+ '''
+import pandas as pd
+
+# wc for women clothing data
+wc = pd.read_csv('Womens Clothing E-Commerce Reviews.csv')
+print(wc)
